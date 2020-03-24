2cf84692fe767b7659215b0f566db903f59de3ba
Denison Ferreira <denisonferreira@MacBook-Pro-de-Denison.local>
2020-03-24 15:36:37 -0300

    Configurado a parte de cancelamento de encomenda

---
+++ src/database/migrations/20200323123530-create-delivery_problems.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('delivery_problems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      order_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'orders',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('delivery_problems');
+  },
+};
